43e850069794ca547200bb48b7a053763106a561
WorkNicolas <Carl.Nicolas.V.Mendoza@gmail.com>
2024-09-28 23:20:42 -0400

    Project Complete

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,7 +14,7 @@ const Projects = () => {
         <div>
             <h1>Projects</h1>
             <h2>Hotel Reservation System - <a href="https://github.com/jcmsj/hotel/blob/main/doc/Group4_ProjectProposal.pdf">GitHub Link</a></h2>
-            <img src={hotel} alt="Hotel Reservation Ticket" width="560" height="315" />
+            <img src={hotel} alt="Hotel Reservation Ticket" width="50%" />
             <p><span className="bold">Role:</span> Room Creator</p>
             <p><span className="bold">Description:</span> I used Java Swing to create columns of rooms with images and buttons that can be infinitely created. To load images upon visit, threads are used.</p>
             <ul>
@@ -24,7 +24,7 @@ const Projects = () => {
                 <li>Programmed the ability to create and display infinite rooms using grid bag constraints which also leverages threads to load the images on startup</li>
             </ul>
             <h2>PinoyFlix - <a href="https://pinoyflix.onrender.com/">Website</a></h2>
-            <img src={pinoyflix} alt="PinoyFlix Website" width="560" height="315" />
+            <img src={pinoyflix} alt="PinoyFlix Website" width="50%" />
             <p><span className="bold">Role:</span> Login Web Developer</p>
             <p><span className="bold">Description:</span> I used FontAwesome to create the login page.</p>
             <ul>
@@ -32,8 +32,8 @@ const Projects = () => {
                 <li>Leveraged IMDB API to get a list of movies/TV shows</li>
             </ul>
             <h2>Market Trainer - <a href="https://www.youtube.com/watch?v=y59C4L3rEg0">YouTube Link</a></h2>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/y59C4L3rEg0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <img src={market} alt="Market Trainer" width="560" height="315" />
+            <iframe width="50%" height="390px" src="https://www.youtube.com/embed/y59C4L3rEg0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <img src={market} alt="Market Trainer" width="50%" />
             <p><span className="bold">Role:</span> Developer</p>
             <p><span className="bold">Description:</span> I created everything except for the video, presentation, and external documentation.</p>
             <ul>
